0ea1d03ee9fe826dc69f75081cc6ee97cf43f369
DiazFernandito <diaz.fernandito@gmail.com>
2023-03-24 23:34:44 -0300

    Ejercicio 9 - FOR

---
+++ script.js
@@ -0,0 +1,10 @@
+// Opción 1
+for(let step = 0; step < 5; step++){
+    console.log("Vuelta N°:" + step);
+}
+
+// Opción 2
+let step
+for(step = 0; step < 5; step++){
+    console.log("Vuelta N°:" + step);
+}
